9a66f636af4f6266fa1c0c2674d32795da098558
Jaya DeHart <dehartj97@gmail.com>
2021-09-15 15:15:42 -0700

    testing ci/cd

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const gameRoutes = require("./routes/games");
-const bookRoutes = require("./routes/books");
-const notFound = require("./error-handlers/404");
-const generalError = require("./error-handlers/500");
+const gameRoutes = require('./routes/games');
+const bookRoutes = require('./routes/books');
+const notFound = require('./error-handlers/404');
+const generalError = require('./error-handlers/500');
 
 function start(port) {
   app.listen(port, () => {
@@ -12,9 +12,14 @@ function start(port) {
 }
 
 app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.send('it works!');
+});
+
 app.use(gameRoutes);
 app.use(bookRoutes);
-app.use("*", notFound);
+app.use('*', notFound);
 app.use(generalError);
 
 module.exports = {
